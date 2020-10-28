9d8f56802c091b12cffafb01bdd9f87c15c3d525
Samuel Walker <sewalker191@gmail.com>
2020-10-28 12:24:09 -0700

    First part of routes controllers lecture

---
+++ app/controllers/chirps_controller.rb
@@ -0,0 +1,63 @@
+class ChirpsController < ApplicationController
+    def index
+        chirps = Chirp.all
+        render json: chirps
+    end
+
+    def show
+        # this should get the info of one specific chirp and render it
+        chirp = Chirp.find_by(id: params[:id])
+        debugger
+        # chirp = Chirp.find(params[:id])
+        render json: chirp
+    end
+
+    def create
+        # create a new chirp, 
+        # save it to the db,
+        # redirect to show if success, 
+        # render errors if fail
+    
+        # {"body"=>"poikjnertgboijkitrmbtr", "author_id"=>"18"}
+        # body: 'awdawda', author_id: 18
+        # params[:somethiong] = 'banana'
+        chirp = Chirp.new(chirp_params)
+        
+        if chirp.save
+            redirect_to chirp_url(chirp)
+        else
+            render json: chirp.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        # find the chirp we are updating
+        # attempt to update it
+        # if success, redirect to the show
+        # send errors
+        chirp = Chirp.find_by(id: params[:id])
+
+        if chirp.update(chirp_params)
+            redirect_to chirp_url(chirp)
+        else
+            render json: chirp.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        # find the chirp we are destroying
+        # destroy it
+        # redirect to show or index
+        chirp = Chirp.find_by(id: params[:id])
+
+        chirp.destroy
+        debugger
+        redirect_to chirps_url # sends to the index action
+    end
+
+    private
+
+    def chirp_params
+        params.require(:chirp).permit(:body, :author_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #? RESTFUL
+  # verb 'path', to: 'controller#action'
+  # get '/chirps', to: 'chirps#index'
+  # get '/chirps/:id', to: 'chirps#show' # /chirps/12
+  # post '/chirps', to: 'chirps#create'
+  # patch '/chirps/:id', to: 'chirps#update'
+  # delete '/chirps/:id', to: 'chirps#destroy'
+
+  #! NOT RESTFUL
+  # get '/chirps/search/:query', to: 'chirps#search'
+
+  # resources :name_of_resource, options hash
+  resources :chirps, except: [:new, :edit]
+  # resources :chirps, only: [:index, :show, :create, :update, :destroy]
 end
